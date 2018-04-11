1862834425b10b444be2a9154125dc30b56ae0a4
Username <danielf_c_c@hotmail.com>
2018-04-11 18:39:58 -0300

    Se agregaron los metodos. Esta es una rama del proyecto

--- src/pizzeria/Factura.java
+++ src/pizzeria/Factura.java
@@ -10,5 +10,69 @@ package pizzeria;
  * @author utku33
  */
 public class Factura {
+    /**
+     * CAMPOS
+     */
+    private String fechaHoraEmision;
+    private int numero;
+
+    /**
+     * CONSTRUCTORES
+     */
+    public Factura() {
+    }
+
+    public Factura(String fechaHoraEmision, int numero) {
+        this.fechaHoraEmision = fechaHoraEmision;
+        this.numero = numero;
+    }
+
+    /**
+     * METODOS GET
+     */
+    public int getNumero() {
+        return numero;
+    }
     
+    public int getEstado() {
+        return numero;
+    }
+    
+    public void getDetalleFactura() {
+        System.out.println("***************");
+        System.out.println("Fecha y hora de Emision: " + fechaHoraEmision);
+        System.out.println("***************");
+        System.out.println("Numero de factura: " + numero);
+        System.out.println("***************");
+    }
+
+    public String getFechaHoraEmision() {
+        return fechaHoraEmision;
+    }
+    
+    /**
+     * METODOS SET
+     */
+    public int setEstado() {
+        return numero;
+    }
+
+    public void setFechaHoraEmision(String fechaHoraEmision) {
+        this.fechaHoraEmision = fechaHoraEmision;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    /**
+     * OTROS METODOS
+     */
+    public void buscarItemsAFacturar() {
+        
+    }
+    
+    public void calcTotalFactura() {
+        
+    }
 }
