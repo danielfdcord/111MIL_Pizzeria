e70593b61fc21581c9724f69d240ab66060bbf53
Agustin Ardizzone <ardizzone.agustin@gmail.com>
2018-04-11 19:00:22 -0300

    Se agrego atributos y metodos vacios a la clase Pedido

--- src/pizzeria/Pedido.java
+++ src/pizzeria/Pedido.java
@@ -11,4 +11,94 @@ package pizzeria;
  */
 public class Pedido {
     
+    private String nombreCliente;
+    private int numero;
+    private String fechaHoraCreacion;
+    private String fechaHoraEntrega;
+    private Factura factura;
+    private EstadoPedido estadoPedido;
+    private DetallePedido detallePedido;
+
+    public Pedido(String nombreCliente, int numero, String fechaHoraCreacion, String fechaHoraEntrega, Factura factura, EstadoPedido estadoPedido, DetallePedido detallePedido) {
+        this.nombreCliente = nombreCliente;
+        this.numero = numero;
+        this.fechaHoraCreacion = fechaHoraCreacion;
+        this.fechaHoraEntrega = fechaHoraEntrega;
+        this.factura = factura;
+        this.estadoPedido = estadoPedido;
+        this.detallePedido = detallePedido;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getFechaHoraCreacion() {
+        return fechaHoraCreacion;
+    }
+
+    public void setFechaHoraCreacion(String fechaHoraCreacion) {
+        this.fechaHoraCreacion = fechaHoraCreacion;
+    }
+
+    public String getFechaHoraEntrega() {
+        return fechaHoraEntrega;
+    }
+
+    public void setFechaHoraEntrega(String fechaHoraEntrega) {
+        this.fechaHoraEntrega = fechaHoraEntrega;
+    }
+
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    public EstadoPedido getEstadoPedido() {
+        return estadoPedido;
+    }
+
+    public void setEstadoPedido(EstadoPedido estadoPedido) {
+        this.estadoPedido = estadoPedido;
+    }
+
+    public DetallePedido getDetallePedido() {
+        return detallePedido;
+    }
+
+    public void setDetallePedido(DetallePedido detallePedido) {
+        this.detallePedido = detallePedido;
+    }
+
+    public float calcTotalPedido(){
+        return 0;
+    }
+    
+    public void cancelar(){
+        
+    }
+    public void confirmar(){
+        
+    }
+    public void facturar(){
+        
+    }
+    public void terminar(){
+        
+    }
 }
