829358e9ba8c83f24460bab17bd723345bdd53ee
santiago <santiagosalamandri@gmail.com>
2018-04-11 18:49:17 -0300

    Agregamos bien los argumentos y metodos de la clase Pizza

--- src/pizzeria/Pizza.java
+++ src/pizzeria/Pizza.java
@@ -11,6 +11,45 @@ package pizzeria;
  */
 public class Pizza extends Pizzeria {
     private String nombre;
-    private s
+    private float precio;
+    private Pizza pizza;
+    private TamanioPizza tamanio;
+    private TipoPizza tipoPizza;
+    private VariedadPizza variedad;
+
+    public Pizza(String nombre, float precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+    public Pizza getPizza() {
+        return pizza;
+    }
     
+    public TamanioPizza getTamanioPizza() {
+        return tamanio;
+    }
+    
+    public TipoPizza getTipoPizza() {
+        return tipoPizza;
+    }
+    
+    public VariedadPizza getVariedad() {
+        return variedad;
+    }
 }
