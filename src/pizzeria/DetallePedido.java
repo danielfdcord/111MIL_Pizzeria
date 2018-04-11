a920b114e5a81e26f8ab004731f2a4a93d66f8ae
Jose Luis <josebeas24@outlook.com>
2018-04-11 18:16:21 -0300

    agregamos setters y getters y constructor

--- src/pizzeria/DetallePedido.java
+++ src/pizzeria/DetallePedido.java
@@ -13,5 +13,27 @@ public class DetallePedido {
     
     private int cantidad;
     private float precio;
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public DetallePedido(int cantidad, float precio) {
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+    
 }
 
